6db50dedcfd30b9c6b44a075967f0c54e2671fe0
Tom Moor <tom.moor@gmail.com>
2020-02-22 18:04:34 -0800

    chore: Upgrade storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered/react';
 import * as Icons from "../src";
 
 const stories = storiesOf('Icon', module);
@@ -8,6 +9,7 @@ const names = Object.keys(Icons);
 for (const name of names) {
   const Component = Icons[name];
   storiesOf(name, module)
+    .addDecorator(centered)
     .add('default', () => <Component />)
     .add('large', () => <Component size={64} />)
     .add('black', () => <Component black />)
