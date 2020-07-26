545403b9f7903f426ccbb3b992da06839d4c9be0
Tom Moor <tom.moor@gmail.com>
2020-07-26 14:50:45 -0700

    fix: Size of InputIcon

--- src/components/InputIcon.js
+++ src/components/InputIcon.js
@@ -6,7 +6,8 @@ import type { Props } from "./Icon";
 export default function InputIcon(props: Props) {
   return (
     <Icon {...props}>
-      <path d="M9 15H8V17H12V15H11V9H12V7H8V9H9V15ZM7 5H17C18.1046 5 19 5.89543 19 7V17C19 18.1046 18.1046 19 17 19H7C5.89543 19 5 18.1046 5 17V7C5 5.89543 5.89543 5 7 5Z" strokeWidth="2" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M17 6H7C6.44772 6 6 6.44772 6 7V17C6 17.5523 6.44772 18 7 18H17C17.5523 18 18 17.5523 18 17V7C18 6.44772 17.5523 6 17 6ZM7 4H17C18.6569 4 20 5.34315 20 7V17C20 18.6569 18.6569 20 17 20H7C5.34315 20 4 18.6569 4 17V7C4 5.34315 5.34315 4 7 4Z" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M9 9H8V7H12V9H11V15H12V17H8V15H9V9Z" />
     </Icon>
   );
 }
