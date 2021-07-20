c11961ad240ce76438bb4d8e22bd6fa3f1f83fb2
Tom Moor <tom.moor@gmail.com>
2021-07-20 16:44:36 -0400

    feat: New icons for menus

---
+++ src/components/BookmarkIcon.js
@@ -0,0 +1,15 @@
+// @flow
+import React from "react";
+import Icon from "./Icon";
+import type { Props } from "./Icon";
+
+export default function BookmarkIcon(props: Props): React$Element<any> {
+  return (
+    <Icon {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6 6C6 4.89543 6.89543 4 8 4H16C17.1046 4 18 4.89543 18 6V19C18 19.3603 17.8062 19.6927 17.4927 19.8702C17.1792 20.0477 16.7944 20.0429 16.4855 19.8575L12 17.1662L7.5145 19.8575C7.20557 20.0429 6.82081 20.0477 6.5073 19.8702C6.19379 19.6927 6 19.3603 6 19V6ZM16 6L8 6V17.2338L11.4855 15.1425C11.8022 14.9525 12.1978 14.9525 12.5145 15.1425L16 17.2338V6Z" />
+    </Icon>
+  );
+}
---
+++ src/components/DuplicateIcon.js
@@ -0,0 +1,15 @@
+// @flow
+import React from "react";
+import Icon from "./Icon";
+import type { Props } from "./Icon";
+
+export default function DuplicateIcon(props: Props): React$Element<any> {
+  return (
+    <Icon {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12 4C10.8954 4 10 4.89543 10 6V8H6C4.89543 8 4 8.89543 4 10V18C4 19.1046 4.89543 20 6 20H12C13.1046 20 14 19.1046 14 18V16H18C19.1046 16 20 15.1046 20 14V7.41421C20 6.88378 19.7893 6.37507 19.4142 6L18 4.58579C17.6249 4.21071 17.1162 4 16.5858 4H12ZM14 14H18V8H17C16.4477 8 16 7.55228 16 7V6H12V8.58579L13.4142 10C13.7893 10.3751 14 10.8838 14 11.4142V14ZM10 10H6V18H12V15V12H11C10.4477 12 10 11.5523 10 11V10Z" />
+    </Icon>
+  );
+}
