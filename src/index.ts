5303ee76d95e8fbbaf2d58cd1c18e8ace932a2ab
Tom Moor <tom.moor@gmail.com>
2021-12-10 22:29:40 -0800

    feat: Add ArrowIcon, BicycleIcon, TerminalIcon

--- src/index.ts
+++ src/index.ts
@@ -1,221 +1,114 @@
 export { default as AcademicCapIcon } from "./components/AcademicCapIcon";
-
 export { default as AlignCenterIcon } from "./components/AlignCenterIcon";
-
 export { default as AlignImageCenterIcon } from "./components/AlignImageCenterIcon";
-
 export { default as AlignImageLeftIcon } from "./components/AlignImageLeftIcon";
-
 export { default as AlignImageRightIcon } from "./components/AlignImageRightIcon";
-
 export { default as AlignLeftIcon } from "./components/AlignLeftIcon";
-
 export { default as AlignRightIcon } from "./components/AlignRightIcon";
-
 export { default as AlphabeticalSortIcon } from "./components/AlphabeticalSortIcon";
-
 export { default as ArchiveIcon } from "./components/ArchiveIcon";
-
+export { default as ArrowIcon } from "./components/ArrowIcon";
 export { default as BackIcon } from "./components/BackIcon";
-
 export { default as BeakerIcon } from "./components/BeakerIcon";
-
+export { default as BicycleIcon } from "./components/BicycleIcon";
 export { default as BillingIcon } from "./components/BillingIcon";
-
 export { default as BlockQuoteIcon } from "./components/BlockQuoteIcon";
-
 export { default as BoldIcon } from "./components/BoldIcon";
-
 export { default as BookmarkIcon } from "./components/BookmarkIcon";
-
 export { default as BookmarkedIcon } from "./components/BookmarkedIcon";
-
 export { default as BrowserIcon } from "./components/BrowserIcon";
-
 export { default as BugIcon } from "./components/BugIcon";
-
 export { default as BuildingBlocksIcon } from "./components/BuildingBlocksIcon";
-
 export { default as BulletedListIcon } from "./components/BulletedListIcon";
-
 export { default as CameraIcon } from "./components/CameraIcon";
-
 export { default as CheckboxIcon } from "./components/CheckboxIcon";
-
 export { default as CheckmarkIcon } from "./components/CheckmarkIcon";
-
 export { default as CloseIcon } from "./components/CloseIcon";
-
 export { default as CloudIcon } from "./components/CloudIcon";
-
 export { default as CodeIcon } from "./components/CodeIcon";
-
 export { default as CoinsIcon } from "./components/CoinsIcon";
-
 export { default as CollapsedIcon } from "./components/CollapsedIcon";
-
 export { default as CollectionIcon } from "./components/CollectionIcon";
-
 export { default as CommentIcon } from "./components/CommentIcon";
-
 export { default as CopyIcon } from "./components/CopyIcon";
-
 export { default as CrossIcon } from "./components/CrossIcon";
-
 export { default as DisconnectedIcon } from "./components/DisconnectedIcon";
-
 export { default as DocumentIcon } from "./components/DocumentIcon";
-
 export { default as DoneIcon } from "./components/DoneIcon";
-
 export { default as DownloadIcon } from "./components/DownloadIcon";
-
 export { default as DuplicateIcon } from "./components/DuplicateIcon";
-
 export { default as EditIcon } from "./components/EditIcon";
-
 export { default as EmailIcon } from "./components/EmailIcon";
-
 export { default as ExpandedIcon } from "./components/ExpandedIcon";
-
 export { default as ExportIcon } from "./components/ExportIcon";
-
 export { default as EyeIcon } from "./components/EyeIcon";
-
 export { default as FeedbackIcon } from "./components/FeedbackIcon";
-
 export { default as GlobeIcon } from "./components/GlobeIcon";
-
 export { default as GoToIcon } from "./components/GoToIcon";
-
 export { default as GroupIcon } from "./components/GroupIcon";
-
 export { default as HashtagIcon } from "./components/HashtagIcon";
-
 export { default as Heading1Icon } from "./components/Heading1Icon";
-
 export { default as Heading2Icon } from "./components/Heading2Icon";
-
 export { default as Heading3Icon } from "./components/Heading3Icon";
-
 export { default as HighlightIcon } from "./components/HighlightIcon";
-
 export { default as HistoryIcon } from "./components/HistoryIcon";
-
 export { default as HomeIcon } from "./components/HomeIcon";
-
 export { default as HorizontalRuleIcon } from "./components/HorizontalRuleIcon";
-
 export { default as Icon } from "./components/Icon";
-
 export { default as ImageIcon } from "./components/ImageIcon";
-
 export { default as ImportIcon } from "./components/ImportIcon";
-
 export { default as InfoIcon } from "./components/InfoIcon";
-
 export { default as InputIcon } from "./components/InputIcon";
-
 export { default as InsertAboveIcon } from "./components/InsertAboveIcon";
-
 export { default as InsertBelowIcon } from "./components/InsertBelowIcon";
-
 export { default as InsertLeftIcon } from "./components/InsertLeftIcon";
-
 export { default as InsertRightIcon } from "./components/InsertRightIcon";
-
 export { default as ItalicIcon } from "./components/ItalicIcon";
-
 export { default as JournalIcon } from "./components/JournalIcon";
-
 export { default as KeyboardIcon } from "./components/KeyboardIcon";
-
 export { default as LeafIcon } from "./components/LeafIcon";
-
 export { default as LightBulbIcon } from "./components/LightBulbIcon";
-
 export { default as LinkIcon } from "./components/LinkIcon";
-
 export { default as ManualSortIcon } from "./components/ManualSortIcon";
-
 export { default as MathIcon } from "./components/MathIcon";
-
 export { default as MenuIcon } from "./components/MenuIcon";
-
 export { default as MoonIcon } from "./components/MoonIcon";
-
 export { default as MoreIcon } from "./components/MoreIcon";
-
 export { default as MoveIcon } from "./components/MoveIcon";
-
 export { default as NewDocumentIcon } from "./components/NewDocumentIcon";
-
 export { default as NextIcon } from "./components/NextIcon";
-
 export { default as NotepadIcon } from "./components/NotepadIcon";
-
 export { default as OpenIcon } from "./components/OpenIcon";
-
 export { default as OrderedListIcon } from "./components/OrderedListIcon";
-
 export { default as PadlockIcon } from "./components/PadlockIcon";
-
 export { default as PageBreakIcon } from "./components/PageBreakIcon";
-
 export { default as PaletteIcon } from "./components/PaletteIcon";
-
 export { default as PinIcon } from "./components/PinIcon";
-
 export { default as PlusIcon } from "./components/PlusIcon";
-
 export { default as PrintIcon } from "./components/PrintIcon";
-
 export { default as PrivateCollectionIcon } from "./components/PrivateCollectionIcon";
-
 export { default as ProfileIcon } from "./components/ProfileIcon";
-
 export { default as PromoteIcon } from "./components/PromoteIcon";
-
 export { default as PublishIcon } from "./components/PublishIcon";
-
 export { default as QuestionMarkIcon } from "./components/QuestionMarkIcon";
-
 export { default as RestoreIcon } from "./components/RestoreIcon";
-
 export { default as SearchIcon } from "./components/SearchIcon";
-
 export { default as SettingsIcon } from "./components/SettingsIcon";
-
 export { default as ShapesIcon } from "./components/ShapesIcon";
-
 export { default as SportIcon } from "./components/SportIcon";
-
 export { default as StarredIcon } from "./components/StarredIcon";
-
 export { default as StrikethroughIcon } from "./components/StrikethroughIcon";
-
 export { default as SunIcon } from "./components/SunIcon";
-
 export { default as TableIcon } from "./components/TableIcon";
-
 export { default as TableOfContentsIcon } from "./components/TableOfContentsIcon";
-
 export { default as TargetIcon } from "./components/TargetIcon";
-
 export { default as TeamIcon } from "./components/TeamIcon";
-
+export { default as TerminalIcon } from "./components/TerminalIcon";
 export { default as TodoListIcon } from "./components/TodoListIcon";
-
 export { default as ToolsIcon } from "./components/ToolsIcon";
-
 export { default as TrashIcon } from "./components/TrashIcon";
-
 export { default as UnpublishIcon } from "./components/UnpublishIcon";
-
 export { default as UnstarredIcon } from "./components/UnstarredIcon";
-
 export { default as UserIcon } from "./components/UserIcon";
-
 export { default as VehicleIcon } from "./components/VehicleIcon";
-
 export { default as WarningIcon } from "./components/WarningIcon";
